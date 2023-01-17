93ea9b4f66430733a01458590c19389e0193cf28
Gulkaiyr_Bakytbekova <bakytbekova6869@gmail.com>
2023-01-17 19:40:05 +0600

    Пересдача

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,16 +1,39 @@
 package task2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите числа:");
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-
-
+    static <E> List<E> noDuplicate(E[] array) {
+        List<E> list = new ArrayList<>();
+        Map<E, E> map = new HashMap<>();
+        for (E arr : array) {
+            map.put(arr, arr);
+        }
+        Set<Map.Entry<E, E>> set = map.entrySet();
+        for (Map.Entry<E, E> st : set) {
+            list.add(st.getKey());
+        }
+        return list;
     }
 
+    static List<String> noDuplicate(String value) {
+        String text = value;
+        text = text.replace(" ", "");
+        String[] arrayString = text.split(",");
+
+        return Main.<String>noDuplicate(arrayString);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Формат чисел : 1,2,3,3,4... и т.д");
+        System.out.print("Введите набор чисел : ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        List<String> listString = Main.<String>noDuplicate(text);
+
+        System.out.println(text);
+        System.out.println(Arrays.toString(listString.toArray()));
+    }
 
 
 }
